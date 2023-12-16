42681e348f21594b30c3e898c65eb5304310dbd5
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-12-16 08:49:40 -0300

    Aula 23: Estudo inicial usando o console

---
+++ curso_java/src/secao04/aula23.java
@@ -0,0 +1,14 @@
+package secao04;
+
+public class aula23 {
+
+	public static void main(String[] args) {
+		
+		// Saídas no Java
+		
+		System.out.println("Olá mundo");
+		System.out.println("Sou outra saída");
+
+	}
+
+}
